5b5016bb8d5a31f5ad26593169dc514f4bf251d6
Chris <banksjazz87@gmail.com>
2023-04-12 13:38:47 -0400

    chore: I have set up the front end interface of the login form

--- client/src/views/Login.tsx
+++ client/src/views/Login.tsx
@@ -1,12 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../components/Navbar.tsx";
+
+interface User {
+    name: string,
+    password: string
+};
 
 
 export default function Login() {
+    
+    const [login, setLogin] = useState<User>({name: "", password: ""});
+
+    const inputChange = (input :string, field :string) :void => {
+        setLogin({...login, [field]: input});
+    }
+    
     return (
         <div>
             <Navbar />
             <h1>This Will be the login page. </h1>
+            <form
+                onSubmit={(e: React.FormEvent) :void => {
+                    e.preventDefault();
+                    console.log(login);
+                }}>
+                <div className="input_pair">
+                    <label htmlFor="username">Username</label>
+                    <input 
+                        id="username" 
+                        name="username" 
+                        type='text'
+                        onChange={(e): void => inputChange(e.target.value, 'name')}
+                    />
+                </div>
+                <div className="input_pair">
+                    <label htmlFor="user_password">Password</label>
+                    <input 
+                        id="user_password" 
+                        name="user_password" 
+                        type='password'
+                        onChange={(e): void => inputChange(e.target.value, 'password')}
+                    />
+                </div>
+                <div className="input_pair">
+                    <input type="submit" value="Submit" />
+                </div> 
+            </form>
         </div>
     );
 }
