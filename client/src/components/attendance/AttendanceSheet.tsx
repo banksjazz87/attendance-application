63537d2abb1d4f5035b3ee0ee5ceaa33ebc0e4d3
Chris <banksjazz87@gmail.com>
2023-04-18 14:42:27 -0400

    feature: I have created a component to display the attendance sheet

--- client/src/components/attendance/AttendanceSheet.tsx
+++ client/src/components/attendance/AttendanceSheet.tsx
@@ -1,7 +1,86 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
+
+interface Attendee {
+  firstName: string;
+  lastName: string;
+  adult: number;
+  child: number;
+  teen: number;
+  present: number;
+}
 
 export default function AttendanceSheet() {
+  //Dummy Data
+  const attendantData: Attendee[] = [
+    {
+      firstName: "Chuck",
+      lastName: "Barley",
+      adult: 0,
+      child: 0,
+      teen: 1,
+      present: 0,
+    },
+    {
+      firstName: "Chuck",
+      lastName: "Berry",
+      adult: 1,
+      child: 0,
+      teen: 0,
+      present: 0,
+    },
+    {
+      firstName: "Chuck",
+      lastName: "Larry",
+      adult: 1,
+      child: 0,
+      teen: 0,
+      present: 0,
+    },
+  ];
+
+  const [memberData, setMemberData] = useState<Attendee[]>(attendantData);
+
+  const clickHandler = (
+    e: React.MouseEvent<HTMLButtonElement>,
+    index: number
+  ): void => {
+    const copy = memberData.slice();
+
+    if (copy[index].present === 0) {
+      copy[index].present = 1;
+      setMemberData(copy);
+      console.log(memberData[index].lastName, memberData[index].present);
+    } else {
+      copy[index].present = 0;
+      setMemberData(copy);
+      console.log(memberData[index].lastName, memberData[index].present);
+    }
+  };
+
+  const dataPoints = memberData.map((x: Attendee, y: number) => {
     return (
-        <h1>Attendance Sheet</h1>
-    )
+      <tr key={`attendant_row_${y}`} id={`attendant_row_${y}`}>
+        <td>{x.lastName}</td>
+        <td>{x.firstName}</td>
+        <td>
+          <button type="button" onClick={(e) => clickHandler(e, y)}>
+            {x.present === 0 ? "Not Present" : "Present"}
+          </button>
+        </td>
+      </tr>
+    );
+  });
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Last Name</th>
+          <th>First Name</th>
+          <th>Present</th>
+        </tr>
+        {dataPoints}
+      </tbody>
+    </table>
+  );
 }
--- client/src/views/Attendance.tsx
+++ client/src/views/Attendance.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../components/global/Navbar.tsx";
 import GroupDropDown from "../components/global/GroupDropDown.tsx";
+import AttendanceSheet from "../components/attendance/AttendanceSheet.tsx"
 
 
 export default function Attendance() {
@@ -9,6 +10,7 @@ export default function Attendance() {
             <Navbar />
             <h1>This Will be the attendance page. </h1>
             <GroupDropDown />
+            <AttendanceSheet />
         </div>
     );
 }
