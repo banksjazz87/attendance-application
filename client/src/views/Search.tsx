1afae388afc19a6f539521351506dece16e38f5a
Chris <banksjazz87@gmail.com>
2023-05-31 15:19:10 -0400

    feature: I have started working on creating some functionality for looking up all of the attendance sheets in the search page

--- client/src/views/Search.tsx
+++ client/src/views/Search.tsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/global/Navbar.tsx";
 import GroupDropDown from "../components/global/GroupDropDown.tsx";
+import { Group } from "../types/interfaces.ts";
 
 export default function Search() {
+  const [groupTable, setGroupTable] = useState<Group>({
+    name: "",
+    age_group: "",
+    displayName: "",
+  });
+
+  const dropDownChangeHandler = (arr: Group[], value: string): void => {
+    let index = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].displayName === value) {
+        index = i;
+      }
+    }
+
+    setGroupTable({
+      ...groupTable,
+      name: arr[index]["name"],
+      age_group: arr[index]["age_group"],
+      displayName: value,
+    });
+
+    console.log(groupTable);
+  };
+
   return (
     <div>
       <Navbar />
       <h1>This Will be the search page. </h1>
-      <GroupDropDown />
+      <form
+        method="GET"
+        action="/all/attendance-sheets/parent/"
+        onSubmit={(e: React.FormEvent<HTMLFormElement>): void => {
+          e.preventDefault();
+        }}
+      >
+        <GroupDropDown attendanceGroupSelected={dropDownChangeHandler} />
+        <input
+          type="submit"
+          value="submit"
+        />
+      </form>
     </div>
   );
 }
