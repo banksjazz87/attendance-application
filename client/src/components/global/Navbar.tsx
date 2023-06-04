b7c82522470e6c5fbe6f0fbc5ce3d4e4cd3f752d
Chris <banksjazz87@gmail.com>
2023-06-04 09:55:46 -0400

    style: I have the icons and the general layout for the navbar finished

--- client/src/components/global/Navbar.tsx
+++ client/src/components/global/Navbar.tsx
@@ -1,28 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../../assets/styles/components/navbar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faKey, faChartLine, faFileCirclePlus, faFile, faSearch, faPeopleGroup } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
     <nav id="main_nav_wrapper">
       <ul>
         <li>
-          <Link to={`/`}>Login</Link>
+          <Link to={`/`}>
+            {" "}
+            <FontAwesomeIcon icon={faKey} />
+          </Link>
         </li>
         <li>
-          <Link to={`/dashboard`}>Dashboard</Link>
+          <Link to={`/dashboard`}>
+            <FontAwesomeIcon icon={faChartLine} />
+          </Link>
         </li>
         <li>
-          <Link to={`/new-attendance`}>New Attendance</Link>
+          <Link to={`/new-attendance`}>
+            <FontAwesomeIcon icon={faFileCirclePlus} />
+          </Link>
         </li>
         <li>
-          <Link to={`/attendance`}>Attendance</Link>
+          <Link to={`/attendance`}>
+            <FontAwesomeIcon icon={faFile} />
+          </Link>
         </li>
         <li>
-          <Link to={`/search`}>Search</Link>
+          <Link to={`/search`}>
+            <FontAwesomeIcon icon={faSearch} />
+          </Link>
         </li>
         <li>
-          <Link to={`/people`}>People</Link>
+          <Link to={`/people`}>
+            <FontAwesomeIcon icon={faPeopleGroup} />
+          </Link>
         </li>
       </ul>
     </nav>
