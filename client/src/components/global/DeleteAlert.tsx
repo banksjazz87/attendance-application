ccaab413d4a7200d0226efeae34e7d73d2139c05
Chris <banksjazz87@gmail.com>
2023-06-23 14:20:50 -0400

    style: I styled the delete alert component

--- client/src/components/global/DeleteAlert.tsx
+++ client/src/components/global/DeleteAlert.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { DeleteProps, DeleteResponse } from "../../types/interfaces.ts";
 import deleteData from "../../functions/api/delete.ts";
+import "../../assets/styles/components/global/deleteAlert.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
 
-
-export default function DeleteAlert({message, hideHandler, url, show,deleteUser,}: DeleteProps) {
-
-  const deletePerson = (
-    event: React.PointerEvent<HTMLButtonElement>,
-  ): void => {
+export default function DeleteAlert({ message, hideHandler, url, show, deleteUser }: DeleteProps) {
+  const deletePerson = (event: React.PointerEvent<HTMLButtonElement>): void => {
     if (deleteUser) {
       deleteData(url).then((data: DeleteResponse): void => {
         if (data.message === "failure") {
-            alert(data.error);
+          alert(data.error);
         } else {
-            alert(data.message);
-            window.location.reload();
+          alert(data.message);
+          window.location.reload();
         }
       });
     }
@@ -22,19 +21,35 @@ export default function DeleteAlert({message, hideHandler, url, show,deleteUser,
 
   const hideAlert = (e: React.PointerEvent<HTMLButtonElement>): void => {
     hideHandler();
-  }
+  };
 
   return (
-    <div id="delete_alert" style={show ? { display: "" } : { display: "none" }}>
-      <p>{message}</p>
-      <div className="button_wrapper">
-        <button type="button" onClick={deletePerson}>Yes</button>
+    <div
+      className="delete_alert_wrapper"
+      style={show ? { display: "" } : { display: "none" }}
+    >
+      <div className="delete_alert">
         <button
-          type="button"
+          className="close_btn"
           onClick={hideAlert}
         >
-          No
+          <FontAwesomeIcon icon={faClose} />
         </button>
+        <p>{message}</p>
+        <div className="button_wrapper">
+          <button
+            type="button"
+            onClick={deletePerson}
+          >
+            Yes
+          </button>
+          <button
+            type="button"
+            onClick={hideAlert}
+          >
+            No
+          </button>
+        </div>
       </div>
     </div>
   );
