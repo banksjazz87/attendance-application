f4366f504df5c0416a92c446c4c2948c89ae4838
Chris <banksjazz87@gmail.com>
2023-08-18 22:57:45 -0400

    style: updated the style for the active box shadow on the input field

--- client/src/components/global/SearchBar.tsx
+++ client/src/components/global/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,  {useState, useEffect} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "../../assets/styles/components/global/searchBar.scss";
@@ -7,18 +7,38 @@ interface SearchBarProps {
   updatePartial: Function;
 }
 export default function SearchBar({ updatePartial }: SearchBarProps): JSX.Element {
-  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    let target = e.target as HTMLInputElement;
+  
+    const [activeSearch, setSearchActive] = useState<boolean>(false);
 
-    if (target) {
-      updatePartial(target.value);
-    }
-  };
+    useEffect(() => {
+        
+        window.addEventListener('click', (e) => {
+            let target = e.target as HTMLElement;
+            if (target.id === 'search_term') {
+                setSearchActive(true);
+                console.log('Active', target.id);
+            } else {
+                setSearchActive(false);
+                console.log('inactive', target.id);
+            }
+        });
+    })
+
+    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        
+        let target = e.target as HTMLInputElement;
+        updatePartial(target.value);
+        setSearchActive(true);
+   
+    };
+
+
 
   return (
     <div id="search_form_wrapper">
       <label htmlFor="search_term">Search</label>
-      <div id="inner_search_wrapper">
+      <div id="inner_search_wrapper" 
+      style={activeSearch ? {boxShadow: "0 2px 6px rgba(238, 108, 77, .6)"} : {boxShadow: "0 1px 4px rgba(41, 50, 65, .6)" }}>
         <FontAwesomeIcon
           className="search"
           icon={faSearch}
