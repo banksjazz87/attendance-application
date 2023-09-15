998dd363b53b1504833ba6985e000dea298b9768
Chris <banksjazz87@gmail.com>
2023-09-14 21:36:03 -0400

    feature: I have a function in place that will modify the title of the attendance to be a digital representation of the month day year for the graph

--- client/src/components/dashboard/DataGraph.tsx
+++ client/src/components/dashboard/DataGraph.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { AttendanceTotals } from "../../types/interfaces.ts";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar } from "recharts";
 import "../../assets/styles/components/dashboard/dataGraph.scss";
 
 interface DataGraphProps {
@@ -18,11 +18,36 @@ interface DataGraphSet {
 }
 
 export default function DataGraph({ allData, month }: DataGraphProps): JSX.Element {
-	const neededData = (arr: AttendanceTotals[]): DataGraphSet[] => {
+    
+    const returnMonthDay = (str: string): string => {
+        let months = ['', "January", "February", 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+        //Create an array of the string
+        let arrayOfStr = str.split(' ');
+
+        //Get the last two of the year
+        let year = arrayOfStr.slice(2).toString();
+        let arrYear = year.split('');
+        let lastTwoOfYear = arrYear.slice(2).join('');
+
+        //Just get the month and day
+        let noYear = arrayOfStr.slice(0, 2);
+
+        //Replace the text of the month to a number
+        noYear.splice(0, 1, months.indexOf(noYear[0]).toString());
+
+        //Add the last two of the year to the end.
+        noYear.push(lastTwoOfYear);
+
+        let addDash = noYear.join('/');
+        return addDash;
+    }
+    
+    const neededData = (arr: AttendanceTotals[]): DataGraphSet[] => {
 		let array = [];
 		for (let i = 0; i < arr.length; i++) {
 			let dataSet = {
-				name: arr[i].displayTitle,
+				name: returnMonthDay(arr[i].displayTitle),
 				children: arr[i].totalChildren,
 				youth: arr[i].totalYouth,
 				adults: arr[i].totalAdults,
@@ -33,7 +58,6 @@ export default function DataGraph({ allData, month }: DataGraphProps): JSX.Eleme
 			array.push(dataSet);
 		}
 
-		console.log("original one", array);
 		return array;
 	};
 
@@ -42,6 +66,7 @@ export default function DataGraph({ allData, month }: DataGraphProps): JSX.Eleme
 		let finalStr = value.replace(regex, " ");
 		return finalStr;
 	}
+
 
 	return (
 		<div
@@ -98,6 +123,29 @@ export default function DataGraph({ allData, month }: DataGraphProps): JSX.Eleme
 					strokeWidth="2"
 				/>
 			</LineChart>
+
+            <BarChart
+          width={500}
+          height={300}
+          data={neededData(allData)}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="total" fill="#8884d8" />
+          <Bar dataKey="visitors" fill="#82ca9d" />
+          <Bar dataKey="children" fill="#8884d8" />
+          <Bar dataKey="youth" fill="#82ca9d" />
+          <Bar dataKey="adults" fill="#82ca9d" />
+        </BarChart>
 		</div>
 	);
 }
