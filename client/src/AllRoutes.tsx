94b36281a7c3b3e6d93581af012e373c6a1c43a4
Chris <banksjazz87@gmail.com>
2024-04-18 08:17:22 -0400

    feature: added a new view for the visitors and started addressing some issues with a few of the buttons I have noticed

--- client/src/AllRoutes.tsx
+++ client/src/AllRoutes.tsx
@@ -5,6 +5,7 @@ import Search from "./views/Search.tsx";
 import NewAttendance from "./views/NewAttendance.tsx";
 import Dashboard from "./views/Dashboard.tsx";
 import People from "./views/People.tsx";
+import Visitors from "./views/Visitors.tsx";
 
 export default function AllRoutes(): JSX.Element {
   return (
@@ -33,6 +34,10 @@ export default function AllRoutes(): JSX.Element {
         <Route
           path="/people"
           element={<People />}
+        />
+        <Route
+          path="/visitors"
+          element={<Visitors />}
         />
       </Routes>
     </BrowserRouter>
---
+++ client/src/views/Visitors.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/global/Navbar.tsx";
+
+
+export default function Vistors() {
+    return (
+        <div id="visitors_page_wrapper">
+            <Navbar />
+            <div className="header_wrapper">
+				<h1>Visitors</h1>
+			</div>
+        </div>
+    )
+}
