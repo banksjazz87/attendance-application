56c0a21aca58f0a15a851eb711b5b76708cdf654
Chris <banksjazz87@gmail.com>
2024-04-29 15:16:29 -0400

    style: working on styling the visitor form

--- client/src/components/visitors/VisitorModal.tsx
+++ client/src/components/visitors/VisitorModal.tsx
@@ -5,95 +5,100 @@ import { AllVisitorData, VisitorChildren, VisitorInterests, VisitorSpouse } from
 import VisitorRow from "../visitors/VisitorRow";
 import "../../assets/styles/components/visitors/visitorModal.scss";
 
-
 interface VisitorModalProps {
-    showModal: boolean;
-    hideModal: Function;
-    formData: AllVisitorData;
+	showModal: boolean;
+	hideModal: Function;
+	formData: AllVisitorData;
 }
 
-export default function VisitorModal({showModal, hideModal, formData}: VisitorModalProps): JSX.Element {
-
-    const displayChildren: JSX.Element[] = formData.children.map((x: VisitorChildren, y: number): JSX.Element => {
-        return (
-           <p key={`child_${y}`}>{`${x.firstName} ${x.lastName}`}</p>
-        );
-    });
-
-    const displayInterests: JSX.Element[] = formData.interests.map((x: VisitorInterests, y: number): JSX.Element => {
-        return (
-            <p key={`interest_${y}`}>{x.interest}</p>
-        );
-    });
-
-    const displaySpouse: JSX.Element[] = formData.spouse.map((x: VisitorSpouse, y: number): JSX.Element => {
-        return (
-            <p key={`spouse_${y}`}>{`${x.firstName} ${x.lastName}`}</p>
-        );
-    });
-
+export default function VisitorModal({ showModal, hideModal, formData }: VisitorModalProps): JSX.Element {
     const form = formData.form[0];
+	const spouse = formData.spouse[0];
 
+	const displayChildren: JSX.Element[] = formData.children.map((x: VisitorChildren, y: number): JSX.Element => {
+		return <p key={`child_${y}`}>{`${x.firstName} ${x.lastName}`}</p>;
+	});
 
-    return (
-        <div 
-        className="full_height_popout"
-        style={showModal ? {display: ""}: {display: "none"}}
-        >
-            <div id="visitor_modal_wrapper">
-                <button 
-                    className='close_btn'
-                    onClick={(e: React.MouseEvent<HTMLButtonElement>) => hideModal()}>
-                    <FontAwesomeIcon icon={faClose} />
-                </button>
-                <div id="visitor_modal_content">
-                    <h3>Visitor Form</h3>
-                    <VisitorRow 
-                        title="Name"
-                        text={`${form.title} ${form.firstName} ${form.lastName}`}
-                    />
-                    <div>
-                        <p>Spouse</p>
-                        <div className="horizontal_list">
-                            {displaySpouse}
-                        </div>
-                    </div>
-                    <div>
-                        <p>Children</p>
-                        <div className="vertical_list">
-                            {displayChildren}
-                        </div>
-                    </div>
-                    <VisitorRow 
-                        title="Address"
-                        text={`${form.address} \n ${form.city}, ${form.state}`}
-                    />
-                    <VisitorRow 
-                        title="Phone"
-                        text={`${form.phone}`}
-                        linkURL={`tel:${form.phone}`}
-                    />
-                    <VisitorRow 
-                        title="Email"
-                        text={`${form.phone}`}
-                        linkURL={`mailto:${form.email}`}
-                    />
-                    <VisitorRow 
-                        title="Preferred Contact"
-                        text={`${form.contact_method}`}
-                    />
-                    <div>
-                        <p>Interests</p>
-                        <div className="vertical_list">
-                            {displayInterests}
-                        </div>
-                    </div>
-                    <VisitorRow
-                        title="Prayer Requests"
-                        text={form.prayer_requests}
-                   />
-                </div>
-            </div>
-        </div>
-    )
+	const displayInterests: JSX.Element[] = formData.interests.map((x: VisitorInterests, y: number): JSX.Element => {
+		return <p key={`interest_${y}`}>{x.interest}</p>;
+	});
+
+	return (
+		<div
+			className="full_height_popout"
+			style={showModal ? { display: "" } : { display: "none" }}
+		>
+			<div id="visitor_modal_wrapper">
+				<button
+					className="close_btn"
+					onClick={(e: React.MouseEvent<HTMLButtonElement>) => hideModal()}
+				>
+					<FontAwesomeIcon icon={faClose} />
+				</button>
+				<div id="visitor_modal_content">
+					<table>
+						<thead>
+							<tr>
+								<th colSpan={2}><h2>Visitor Form</h2></th>
+							</tr>
+						</thead>
+						<tbody>
+							<VisitorRow
+								title="Name"
+								text={`${form.title} ${form.firstName} ${form.lastName}`}
+							/>
+							<VisitorRow
+								title="Spouse"
+								text={`${spouse.firstName} ${spouse.lastName}`}
+							/>
+							<tr>
+								<td className="title">
+									<h3>Children</h3>
+								</td>
+								<td className="title">
+									<div className="vertical_data">{displayChildren}</div>
+								</td>
+							</tr>
+							<VisitorRow
+								title="Address"
+								text={`${form.address} \n ${form.city}, ${form.state}`}
+							/>
+							<VisitorRow
+								title="Phone"
+								text={`${form.phone}`}
+								linkURL={`tel:${form.phone}`}
+							/>
+							<VisitorRow
+								title="Email"
+								text={`${form.phone}`}
+								linkURL={`mailto:${form.email}`}
+							/>
+							<VisitorRow
+								title="Preferred Contact"
+								text={`${form.contact_method}`}
+							/>
+							<tr>
+								<td className="title">
+									<h3>Interests</h3>
+								</td>
+								<div className="vertical_data">{displayInterests}</div>
+							</tr>
+							<VisitorRow
+								title="Prayer Requests"
+								text={form.prayer_requests}
+							/>
+						</tbody>
+                        <tfoot>
+                            <tr>
+                                <td>
+                                    <p>{`This form was submitted on ${form.dateCreated}`}</p>
+                                </td>
+                            </tr>
+                        </tfoot>
+					</table>
+
+				</div>
+			</div>
+		</div>
+	);
 }
--- client/src/components/visitors/VisitorRow.tsx
+++ client/src/components/visitors/VisitorRow.tsx
@@ -1,28 +1,40 @@
 import React from "react";
 
 interface VisitorRowProps {
-    title: string;
-    text: string;
-    linkURL?: string;
+	title: string;
+	text: string;
+	linkURL?: string;
 }
 
-
-export default function VisitorRow({title, text, linkURL}: VisitorRowProps) {
-
-    if (linkURL) {
-        return (
-        <div className="visitorRow">
-            <p>{title}</p>
-            <a href={linkURL} target="_blank" rel="noreferrer">{text}</a>
-        </div>
-        );
-
-    } else {
-        return (
-            <div className="visitorRow">
-                <p>{title}</p>
-                <p>{text}</p>
-            </div>
-        );
-    }
+export default function VisitorRow({ title, text, linkURL }: VisitorRowProps) {
+	if (linkURL) {
+		return (
+			<tr className="visitor_row">
+				<td>
+					<h3 className="title">{title}</h3>
+				</td>
+				<td>
+					<a
+						href={linkURL}
+						target="_blank"
+						rel="noreferrer"
+						className="link_text"
+					>
+						{text}
+					</a>
+				</td>
+			</tr>
+		);
+	} else {
+		return (
+			<tr className="visitor_row">
+				<td>
+					<h3 className="title">{title}</h3>
+				</td>
+				<td>
+					<p className="text">{text}</p>
+				</td>
+			</tr>
+		);
+	}
 }
